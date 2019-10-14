9306b7fad6426265e642526b6a0ebafc40149523
Joseph Hunt <jhunted54@gmail.com>
2019-10-14 09:46:36 -0500

    Updates

---
+++ precache-manifest.d01f637d4f73fbae855d0143072f1d16.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8f14eec80e0d0d664bd5302a42c5219c",
+    "url": "/project-4-assessment/index.html"
+  },
+  {
+    "revision": "073051b2d60575a92fdb",
+    "url": "/project-4-assessment/static/css/main.32db9ca5.chunk.css"
+  },
+  {
+    "revision": "1ac1921c0574c7563a8d",
+    "url": "/project-4-assessment/static/js/2.e339a15a.chunk.js"
+  },
+  {
+    "revision": "073051b2d60575a92fdb",
+    "url": "/project-4-assessment/static/js/main.37dc2882.chunk.js"
+  },
+  {
+    "revision": "196f56ccfa1575b64e7a",
+    "url": "/project-4-assessment/static/js/runtime-main.62433aa6.js"
+  }
+]);
